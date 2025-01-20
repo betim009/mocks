535ff2091a1370e4d7d9fdc2736ab62c17ebc2ac
unknown <betim009@gmail.com>
2025-01-20 13:55:09 -0300

    update

---
+++ javascript/filmes.js
@@ -0,0 +1,44 @@
+const movies = [
+    {
+        id: 1,
+        title: "The Shawshank Redemption",
+        year: 1994,
+        genre: "Drama",
+        poster: "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+        id: 2,
+        title: "The Godfather",
+        year: 1972,
+        genre: "Crime",
+        poster: "https://m.media-amazon.com/images/M/MV5BYTJkNGQyZDgtZDQ0NC00MDM0LWEzZWQtYzUzZDEwMDljZWNjXkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+        id: 3,
+        title: "The Dark Knight",
+        year: 2008,
+        genre: "Action",
+        poster: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg"
+    },
+    {
+        id: 4,
+        title: "Pulp Fiction",
+        year: 1994,
+        genre: "Crime",
+        poster: "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+        id: 5,
+        title: "The Lord of the Rings: The Return of the King",
+        year: 2003,
+        genre: "Fantasy",
+        poster: "https://m.media-amazon.com/images/M/MV5BMTZkMjBjNWMtZGI5OC00MGU0LTk4ZTItODg2NWM3NTVmNWQ4XkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+        id: 6,
+        title: "Forrest Gump",
+        year: 1994,
+        genre: "Drama",
+        poster: "https://m.media-amazon.com/images/M/MV5BNDYwNzVjMTItZmU5YS00YjQ5LTljYjgtMjY2NDVmYWMyNWFmXkEyXkFqcGc@._V1_SX300.jpg"
+    }
+];
