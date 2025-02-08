a9b141c4db96f8f1152568501b8690054e792ec9
unknown <betim009@gmail.com>
2025-02-07 21:07:17 -0300

    update

---
+++ javascript/tibia/capacetes.js
@@ -0,0 +1,32 @@
+export const capacetes = [
+    {
+        id: 1,
+        nome: "Capacete do guerreiro",
+        defesa: 4,
+        imagem: "https://www.tibiawiki.com.br/images/d/de/Warrior_Helmet.gif"
+    },
+    {
+        id: 2,
+        nome: "Capacete do templário",
+        defesa: 5,
+        imagem: "https://www.tibiawiki.com.br/wiki/Arquivo:Crusader_Helmet.gif"
+    },
+    {
+        id: 3,
+        nome: "Capacete imperial",
+        defesa: 6,
+        imagem: "https://www.tibiawiki.com.br/images/9/9e/Royal_Helmet.gif"
+    },
+    {
+        id: 4,
+        nome: "Capacete do capiroto",
+        defesa: 7,
+        imagem: "https://www.tibiawiki.com.br/images/4/4e/Demon_Helmet.gif"
+    },
+    {
+        id: 5,
+        nome: "Capacete da tumba perdida",
+        defesa: 8,
+        imagem: "https://www.tibiawiki.com.br/images/6/6c/Full_Helmet_of_the_Ancients.gif"
+    },
+];
---
+++ javascript/tibia/escudos.js
@@ -0,0 +1,30 @@
+export const escudos = [
+    {
+        id: 1,
+        nome: "Escudo de madeira",
+        defesa: 4,
+        ataque: 0,
+        imagem: "https://www.tibiawiki.com.br/images/5/58/Dwarven_Shield.gif"
+    },
+    {
+        id: 2,
+        nome: "Escudo do gavião branco",
+        defesa: 7,
+        ataque: 0,
+        imagem: "https://www.tibiawiki.com.br/images/2/21/Eagle_Shield.gif"
+    },
+    {
+        id: 3,
+        nome: "Escudo abençoado",
+        defesa: 10,
+        ataque: 0,
+        imagem: "https://www.tibiawiki.com.br/images/1/1f/Blessed_Shield.gif"
+    },
+    {
+        id: 4,
+        nome: "Escudo mágico",
+        defesa: 7,
+        ataque: 3,
+        imagem: "https://www.tibiawiki.com.br/images/0/00/Tempest_Shield.gif"
+    }
+]
