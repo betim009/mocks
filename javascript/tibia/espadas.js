e5200b0a347b688b5b2b260d5475f8f4d8f80c83
unknown <betim009@gmail.com>
2025-02-14 10:32:28 -0300

    update

---
+++ javascript/tibia/espadas.js
@@ -0,0 +1,32 @@
+export const espadas = [
+    { 
+        id: 1, 
+        nome: "Espada de ferro pesada", 
+        ataque: 3, 
+        img: "https://www.tibiawiki.com.br/images/8/87/Broadsword.gif" 
+    },
+    { 
+        id: 2, 
+        nome: "Espada do capiroto", 
+        ataque: 6, 
+        img: "https://www.tibiawiki.com.br/images/5/55/Demonrage_Sword.gif" 
+    },
+    { 
+        id: 3, 
+        nome: "Espada de fogo", 
+        ataque: 12, 
+        img: "https://www.tibiawiki.com.br/images/4/4c/Fire_Sword.gif" 
+    },
+    { 
+        id: 4, 
+        nome: "Espada de aço negro", 
+        ataque: 13, 
+        img: "https://www.tibiawiki.com.br/images/4/4c/Fire_Sword.gif" 
+    },
+    { 
+        id: 5, 
+        nome: "Espada sanguinária", 
+        ataque: 18, 
+        img: "https://www.tibiawiki.com.br/images/f/fb/Sanguine_Razor.gif" 
+    },
+];
