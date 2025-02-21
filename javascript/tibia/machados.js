9538141e60eb0f969653afa5c8060b832b030bd7
unknown <betim009@gmail.com>
2025-02-21 09:34:23 -0300

    update

---
+++ javascript/tibia/machados.js
@@ -0,0 +1,32 @@
+export const espadas = [
+    { 
+        id: 1, 
+        nome: "Machado do Anão", 
+        ataque: 6, 
+        img: "https://www.tibiawiki.com.br/images/d/d4/Ornamented_Axe.gif" 
+    },
+    { 
+        id: 2, 
+        nome: "Machado pesado", 
+        ataque: 6, 
+        img: "https://www.tibiawiki.com.br/images/c/c4/Stonecutter_Axe.gif" 
+    },
+    { 
+        id: 3, 
+        nome: "Machado do titã", 
+        ataque: 12, 
+        img: "https://www.tibiawiki.com.br/images/1/1e/Titan_Axe.gif" 
+    },
+    { 
+        id: 4, 
+        nome: "Machado de fogo", 
+        ataque: 13, 
+        img: "https://www.tibiawiki.com.br/images/3/31/Fire_Axe.gif" 
+    },
+    { 
+        id: 5, 
+        nome: "Machado sanguinario", 
+        ataque: 18, 
+        img: "https://www.tibiawiki.com.br/images/4/44/Phantasmal_Axe.gif" 
+    },
+];
